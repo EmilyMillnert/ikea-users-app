64e270e13fc20c08c9521e877e801eb89a5be48f
EMily MIllnert <emily.millnert@ingka.ikea.com>
2024-10-11 13:28:52 +0200

    added all tests in jest

--- tests/index.test.js
+++ tests/index.test.js
@@ -8,17 +8,29 @@ const {app, server, sequelize } = require('../index');
 
 describe('Test the users endpoints', () => {
 
-  //  beforeAll(async () => {
-  //      await sequelize.sync({ force: true }); // Reset the database before tests
-  //  });
+  beforeAll(async () => {
+  await sequelize.sync({ force: true }); // Reset the database before tests
+  });
 
-    test('It should create a new user', async () => {
-      const newUser = { name: "John Doe", isManager: false, site: "Test Site" };  
-      const response = await request(app).post('/api/users').send(newUser);
-      expect(response.body).toMatchObject(newUser);
-      expect(response.statusCode).toBe(200);
+  test('It should create a new user', async () => {
+    const newUser = { name: "John Doe", isManager: false, site: "Test Site" };  
+    const response = await request(app).post('/api/users').send(newUser);
+    expect(response.body).toMatchObject(newUser);
+    expect(response.statusCode).toBe(200);
    });
   
+   test('should retrieve a list of all users', async() => {
+      const response = await request(app).get('/api/users');
+      expect(response.status).toBe(200);
+      expect(Array.isArray(response.body)).toBe(true);
+   });
+
+   test('It should retrieve user with id:1 create in post test', async () => {
+    const response = await request(app).get('/api/users/1');
+    expect(response.body).toMatchObject({name: 'John Doe'});
+    expect(response.statusCode).toBe(200);
+   });
+
    test('It should update a user', async () => {
     const updateUser = { name: "Jane Doe", isManager: false, site: "Test Site" };  
     const response = await request(app).put('/api/users/1').send(updateUser);
@@ -28,8 +40,14 @@ describe('Test the users endpoints', () => {
 
   test('It should delete a user', async () => {
    const response = await request(app).delete('/api/users/1');
-   expect(response.body).toEqual({id:'1'});
+   expect(response.body).toEqual({data: 'The user with id of 1 is removed.'});
    expect(response.statusCode).toBe(200);
+
+    // Verify that the user is indeed deleted
+    const userResponse = await request(app).get('/api/users/1');
+    expect(userResponse.statusCode).toBe(200);
+    expect(userResponse.body).toBeNull(); // Assuming your API returns null for a non-existent user
+
  });
 
 
